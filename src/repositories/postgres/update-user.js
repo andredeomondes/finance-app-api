9a09d9b71a3538561fa1416f3b39602b370a5f9c
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-04-19 21:32:38 -0300

    feat: adiona PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,33 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresUpdateUserRepository {
+    async execute(
+        userId,
+        updateUserParams = {
+            first_name: 'Felipe',
+        },
+    ) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateUserParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateUserParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE users
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateValues.length}
+            RETURNING id, first_name, last_name, email
+        `
+
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+        return updatedUser.rows[0]
+    }
+}
