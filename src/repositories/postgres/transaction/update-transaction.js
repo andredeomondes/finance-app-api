76be6c0304ed2df36d45f7ee34c831e78eb536e1
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-04-25 12:14:28 -0300

    fix: pass correct params to UpdateTransactionRepository

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,6 +1,6 @@
 import { PostgresHelper } from '../../../db/postgres/helper.js'
 export class PostgresUpdateTransactionRepository {
-    async execute(userId, updateTransactionParams) {
+    async execute(transactionId, updateTransactionParams) {
         const updateFields = []
         const updateValues = []
 
@@ -9,7 +9,7 @@ export class PostgresUpdateTransactionRepository {
             updateValues.push(updateTransactionParams[key])
         })
 
-        updateValues.push(userId)
+        updateValues.push(transactionId)
 
         const updateQuery = `
                     UPDATE transactions
--- src/use-cases/transaction/update-transaction.js
+++ src/use-cases/transaction/update-transaction.js
@@ -1,17 +1,14 @@
-import { UserNotFoundError } from '../../errors/user.js'
 export class UpdateTransactionUseCase {
-    constructor(updateTransactionRepository, getUserByIdRepository) {
+    constructor(updateTransactionRepository) {
         this.updateTransactionRepository = updateTransactionRepository
-        this.getUserByIdRepository = getUserByIdRepository
     }
 
-    async execute(params) {
-        const user = await this.getUserByIdRepository.getUserById(params.userId)
-        if (!user) {
-            throw new UserNotFoundError()
-        }
+    async execute(transactionId, params) {
         const transaction =
-            await this.updateTransactionRepository.updateTransaction(params)
+            await this.updateTransactionRepository.updateTransaction(
+                transactionId,
+                params,
+            )
         return transaction
     }
 }
