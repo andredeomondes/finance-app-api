91b1c33a788be4ed90c7e9c1d5e4528359bd4fb6
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-02 22:42:42 -0300

    feat: ensure CreateUser and GetUserByEmailRepository

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -1,16 +1,17 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        const result = await PostgresHelper.query(
-            'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *;',
-            [
-                createUserParams.first_name,
-                createUserParams.last_name,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        )
-        return result[0]
+        const user = await prisma.user.create({
+            data: {
+                id: createUserParams.id,
+                email: createUserParams.email,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                password: createUserParams.password,
+            },
+        })
+
+        return user
     }
 }
