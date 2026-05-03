b432954779711150c523dd6d7307d182be8a98f2
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-02 23:28:42 -0300

    feat: add Create and Delete Transaction

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,12 +1,15 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
-export class PostgresDeleteTransactionRepository {
-    async execute(transactionId) {
-        const transaction = await PostgresHelper.query(
-            'DELETE FROM transactions WHERE id = $1 RETURNING *',
-            [transactionId],
-        )
-
-        return transaction[0]
+export class PostgresCreateTransactionRepository {
+    async execute(transcationId) {
+        try {
+            await prisma.transaction.delete({
+                where: {
+                    id: transcationId,
+                },
+            })
+        } catch (error) {
+            return null
+        }
     }
 }
