4c3bfd146655a3f9ddd700ab23f2eb6d30107220
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-02 23:24:29 -0300

    feat: ensure GetUserBalance

---
+++ src/repositories/postgres/user/get-user-balance.js
@@ -0,0 +1,54 @@
+import { prisma } from '../../../../prisma/prisma.js'
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const {
+            _sum: { amount: totalExpenses },
+        } = await prisma.transaction.aggregate({
+            where: {
+                userId: userId,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalEarnings },
+        } = await prisma.transaction.aggregate({
+            where: {
+                userId: userId,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalInvestments },
+        } = await prisma.transaction.aggregate({
+            where: {
+                userId: userId,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const _totalEarnings = totalEarnings || 0
+        const _totalExpenses = totalExpenses || 0
+        const _totalInvestments = totalInvestments || 0
+
+        const balance = _totalEarnings - _totalExpenses - _totalInvestments
+
+        return {
+            earnings: _totalEarnings,
+            expenses: _totalExpenses,
+            investments: _totalInvestments,
+            balance,
+        }
+    }
+}
