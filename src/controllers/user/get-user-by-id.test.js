2eb822a5407e4ee23c07d7b09b32b8d1476bbed9
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-06 04:41:00 -0300

    test: ensure GetUserByIdController returns 500 if GetUserByIdUseCase throws an error

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -53,4 +53,16 @@ describe('GetUserByIdController', () => {
 
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 if GetUserByidUseCase throws an error', async () => {
+        const { sut, getUserByIdUseCase } = makeSut()
+
+        jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValue(new Error())
+
+        const result = await sut.execute({
+            params: { userId: faker.string.uuid() },
+        })
+
+        expect(result.statusCode).toBe(500)
+    })
 })
