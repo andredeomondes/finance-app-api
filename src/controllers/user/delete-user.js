5da317660b70f3b69603508b78d8bf9bc2769f07
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-06 03:38:37 -0300

    test: ensure DeleteUserController returns 400

--- src/controllers/user/delete-user.js
+++ src/controllers/user/delete-user.js
@@ -13,17 +13,23 @@ export class DeleteUserController {
     async execute(httpRequest) {
         try {
             const userId = httpRequest.params.userId
+
             const idIsValid = checkIfIdIsValid(userId)
+
             if (!idIsValid) {
                 return generateInvalidIdResponse()
             }
+
             const deletedUser = await this.deleteUserUseCase.execute(userId)
+
             if (!deletedUser || deletedUser.length === 0) {
                 return userNotFoundResponse()
             }
+
             return ok(deletedUser)
         } catch (error) {
             console.error(error)
+
             return serverError()
         }
     }
---
+++ src/controllers/user/delete-user.test.js
@@ -0,0 +1,46 @@
+import { faker } from '@faker-js/faker'
+import { DeleteUserController } from './delete-user'
+
+describe('DeleteUserController', () => {
+    class DeleteUserUseCaseStub {
+        execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            }
+        }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    const makeSut = () => {
+        const deleteUserUseCase = new DeleteUserUseCaseStub()
+        const sut = new DeleteUserController(deleteUserUseCase)
+
+        return { deleteUserUseCase, sut }
+    }
+
+    it('should return 200 if user is deleted', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(200)
+    })
+
+    it('should return 400 if id is invalid', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({ params: { userId: 'invalid_id' } })
+        expect(result.statusCode).toBe(400)
+    })
+})
