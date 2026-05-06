c3b4efa292e1246befc2b7dc143f19e81d613eb0
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-06 05:09:56 -0300

    test: ensure UpdateUserController returns 400 when an unallowed field is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -73,4 +73,18 @@ describe('UpdateUserController', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when an unallowed field is provided', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                unallowed_field: 'unallowed_value',
+            },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
 })
