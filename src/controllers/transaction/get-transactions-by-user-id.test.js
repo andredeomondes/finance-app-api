383cc03707314267e4c1348ddabc3dd5fefa61bd
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-08 03:55:29 -0300

    test: ensure GetTransactionsByUserIdController return 400 when userId param is invalid

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -41,4 +41,14 @@ describe('Get Transactions By User Id Controller', () => {
 
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when userId param is invalid', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            query: { userId: 'invalid_user_id' },
+        })
+
+        expect(response.statusCode).toBe(400)
+    })
 })
