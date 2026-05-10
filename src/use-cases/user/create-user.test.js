5e65318d40692b07813142db98b55e412acec6bf
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-10 19:31:17 -0300

    test: ensure CreateUserUseCase throws if GetUserByemailRepository throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -118,4 +118,18 @@ describe('Create User Use Case', () => {
             id: 'generated_id',
         })
     })
+
+    it('should throw if GetUserByEmailRepository throws', async () => {
+        // Arrange
+        const { sut, getUserByEmailRepository } = makeSut()
+        jest.spyOn(getUserByEmailRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // Act
+        const promise = sut.execute(user)
+
+        // Assert
+        await expect(promise).rejects.toThrow()
+    })
 })
