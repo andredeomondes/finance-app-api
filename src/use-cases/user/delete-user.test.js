38429a4df8cc8390dbf497a55e74eb76219b61ae
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-10 20:45:37 -0300

    test: ensure DeleteUserUseCase calls DeleteUserRepository with correct params

--- src/use-cases/user/delete-user.test.js
+++ src/use-cases/user/delete-user.test.js
@@ -36,4 +36,17 @@ describe('Delete User Use Case', () => {
         // Assert
         expect(deletedUser).toEqual(user)
     })
+
+    it('should call DeleteUserRepository with correct params', async () => {
+        // Arrange
+        const { sut, deleteUserRepository } = makeSut()
+        const executeSpy = jest.spyOn(deleteUserRepository, 'execute')
+        const userId = faker.string.uuid()
+
+        // Act
+        await sut.execute(userId)
+
+        // Assert
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
 })
