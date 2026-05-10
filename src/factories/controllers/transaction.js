5548d682807bdd78c57469e9c6c4dfb5f9b6edef
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-10 17:23:21 -0300

    feat: ensuer CreateTransactionUseCase uses idGeneratorAdapter

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -19,15 +19,20 @@ import {
     DeleteTransactionUseCase,
 } from '../../use-cases/index.js'
 
+import { IdGeneratorAdapter } from '../../adapters/index.js'
+
 export const makeCreateTransactionController = () => {
     const createTransactionRepository =
         new PostgresCreateTransactionRepository()
 
     const getUserByIdRepository = new PostgresGetUserByIdRepository()
 
+    const idGeneratorAdapter = new IdGeneratorAdapter()
+
     const createTransactionUseCase = new CreateTransactionUseCase(
         createTransactionRepository,
         getUserByIdRepository,
+        idGeneratorAdapter,
     )
     const createTransactionController = new CreateTransactionController(
         createTransactionUseCase,
