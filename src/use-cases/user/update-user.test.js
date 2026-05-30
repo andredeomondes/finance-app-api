48c23e319dbde3254592f637e83421d039ba57f0
Andre Deomondes <andredeomondes.dev@gmail.com>
2026-05-30 02:31:20 -0300

    test ensure user can be updated with email and without email and password

---
+++ src/use-cases/user/update-user.test.js
@@ -0,0 +1,81 @@
+import { UpdateUserUseCase } from './update-user'
+import { faker } from '@faker-js/faker'
+
+describe('UpdateUserUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 7,
+        }),
+    }
+    class GetUserByEmailRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async execute() {
+            return 'hashed_password'
+        }
+    }
+
+    class UpdateUserRepositoryStub {
+        async execute() {
+            return
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+        const updateUserRepository = new UpdateUserRepositoryStub()
+        const passwordHasherAdapter = new PasswordHasherAdapterStub()
+        const sut = new UpdateUserUseCase(
+            getUserByEmailRepository,
+            updateUserRepository,
+            passwordHasherAdapter,
+        )
+        return {
+            sut,
+            getUserByEmailRepository,
+            updateUserRepository,
+            passwordHasherAdapter,
+        }
+    }
+
+    it('should update user successfully (without email and password)', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute(faker.string.uuid(), {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+        })
+
+        // Assert
+        expect(result).toBe(user)
+    })
+
+    it('should update user successfully (with email)', async () => {
+        // Arrange
+        const { sut, getUserByEmailRepository } = makeSut()
+        const getUserByEmailRepositorySpy = jest.spyOn(
+            getUserByEmailRepository,
+            'execute',
+        )
+        const email = faker.internet.email()
+
+        // Act
+        const result = await sut.execute(faker.string.uuid(), {
+            email,
+        })
+
+        // Assert
+        expect(getUserByEmailRepositorySpy).toHaveBeenCalledWith(email)
+        expect(result).toBe(user)
+    })
+})
